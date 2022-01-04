6d3c4f5cc199770397629de0ef2dde8902bf0800
hajineys <73228683+hajineys@users.noreply.github.com>
2022-01-05 00:05:17 +0900

    최솟값 구하는 알고리즘
    3, 4개의 숫자를 매개변수로 받아서 비교후 최솟값을 반환하는 함수를 만들었다.
    
    // 필요 없는 파일 삭제

---
+++ Minimum.cpp
@@ -0,0 +1,39 @@
+﻿#include <stdio.h>
+
+/// 세 값의 최솟값을 구하는 함수 min3를 작성하세요.
+int min3(int a, int b, int c)
+{
+	// 초기값 a로 삽입
+	int min = a;
+
+	if (min >= b) min = b;	// b와 비교, b가 min보다 값이 작거나 같다면 교체
+	if (min >= c) min = c;	// c와 비교, c가 ...
+
+	return min;
+}
+
+/// 네 값의 최솟값을 구하는 함수 min4를 작성하세요.
+int min4(int a, int b, int c, int d)
+{
+	// 초기값 a로 삽입
+	int min = a;
+
+	if (min >= b) min = b;	// b와 비교, b가 min보다 값이 작거나 같다면 교체
+	if (min >= c) min = c;	// c와 비교, c가 ...
+	if (min >= d) min = d;	// d와 비교, d가 ...
+
+	return min;
+}
+
+int main(void)
+{
+	int a = 4;
+	int b = 3;
+	int c = 2;
+	int d = 1;
+
+	printf("min3(%d, %d, %d) = %d\n", a, b, c, min3(a, b, c));
+	printf("min4(%d, %d, %d, %d) = %d\n", a, b, c, d, min4(a, b, c, d));
+
+	return 0;
+}

a5da6a990d13918c302cfe6307585d617008cf7f
hajineys <73228683+hajineys@users.noreply.github.com>
2022-01-04 23:45:59 +0900

    최대값 구하는 알고리즘
    숫자 4개를 매개변수로 받아 비교 후 최대값을 반환하는 함수를 만들었다.

---
+++ Maximum/Maximum/main.cpp
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+
+/// 네 값의 최댓값을 구하는 함수 max4를 작성하세요.
+int max4(int a, int b, int c, int d)
+{
+	// 초기값 a로 삽입
+	int max = a;
+
+	if (b >= max) max = b;	// b와 비교, b가 max보다 값이 크거나 같다면 교체
+	if (c >= max) max = c;	// c와 비교, c가 ...
+	if (d >= max) max = d;	// d와 비교, d가 ...
+
+	return max;
+}
+
+int main(void)
+{
+	int a = 4;
+	int b = 3;
+	int c = 2;
+	int d = 1;
+
+	printf("max4(%d, %d, %d, %d) = %d\n", a, b, c, d, max4(a, b, c, d));
+
+	return 0;
+}

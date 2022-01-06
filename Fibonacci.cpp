f5c8ef1b73468a5006ab056d9a42aa3e6c44e15e
hajineys <73228683+hajineys@users.noreply.github.com>
2022-01-07 00:17:00 +0900

    피보나치 수열 구현
    위키백과를 참고하여 피보나치 수열을 재귀호출로 구현하였다.
    
    원하는 n번째 항의 수를 입력받아 차례대로 1번째 항부터 출력한다.

---
+++ Fibonacci.cpp
@@ -0,0 +1,32 @@
+﻿#include <stdio.h>
+
+// 피보나치 수열 구현 함수(위키백과 참고)
+int Fibonacci(int num)
+{
+	int result = 0;
+
+	// 첫째 및 둘째 항은
+	if (num == 0 || num == 1)
+	{
+		return 1;	// 1이다
+	}
+	// 그 뒤의 모든 항은 바로 앞 두 항의 합인 수열이다.
+	else
+	{
+		return Fibonacci(num - 1) + Fibonacci(num - 2);
+	}
+}
+
+int main(void)
+{
+	int number = 0;
+
+	scanf_s("%d", &number);
+
+	// n항에 따른 값을 차례로 출력
+	for (int i = 0; i <= number; i++)
+	{
+		printf("%d", Fibonacci(i));
+		printf(" ");
+	}
+}

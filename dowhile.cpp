0ca0efad1ef924676d560a4a63a7323dc8962544
hajineys <73228683+hajineys@users.noreply.github.com>
2022-01-09 21:40:40 +0900

    do while 연습
    do while은 일단 루프 본문을 한 번 실행한 다음에 계속 반복할 것인지를 판단하는 사후 판단 반복문이다.

---
+++ dowhile.cpp
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+
+/// <summary>
+/// 두 변수 a, b에 정수를 입력하고, b-a를 출력하는 프로그램
+/// 
+/// do문 while(제어식) 사용
+/// </summary>
+int main(void)
+{
+	int a = 0;
+	int b = 0;
+
+	scanf_s("%d", &a);
+	printf("a의 값 : %d\n", a);
+
+	do
+	{
+		scanf_s("%d", &b);
+		printf("b의 값 : %d\n", b);
+
+		if (b < a)
+		{
+			printf("a보다 큰 값을 입력하세요!\n");
+		}
+	} while (b < a);	// b가 a의 값보다 작다면 계속 진행!(계속 조건)
+
+	printf("b - a는 %d입니다.", b - a);
+}

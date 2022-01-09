24e7fdf8176644ce9788a900d403ed929f7bd726
hajineys <73228683+hajineys@users.noreply.github.com>
2022-01-09 21:38:36 +0900

    do while을 활용한 자릿수 출력
    10진법의 개념을 활용하여 값이 0이 나올때까지 10을 나누면 몫을 통해 자릿수를 알 수 있다.
    
    10자리까지 알 수 있었다.
    11자리로 넘어가면 int가 그 값을 담지 못하는 것 같다.

---
+++ PositionalNumber.cpp
@@ -0,0 +1,35 @@
+﻿#include <stdio.h>
+
+/// <summary>
+/// 양의 정수를 입력하고 자릿수를 출력하는 프로그램
+/// </summary>
+int main(void)
+{
+	int number = 0;
+	int digits = 0;
+
+	do
+	{
+		scanf_s("%d", &number);
+
+		if (0 <= number)
+		{
+			printf("양의 정수 : %d\n", number);
+		}
+		else
+		{
+			printf("그 수는 양의 정수가 아닙니다. 다시 입력하세요\n");
+		}
+	} while (number <= 0);
+
+	while (number > 0)
+	{
+		// 입력한 숫자가 0보다 작아질때까지 계속 10을 나눈다.
+		number /= 10;
+
+		// 숫자의 자릿수 만큼 +1이 된다.
+		digits++;
+	}
+
+	printf("그 수는 %d자리입니다.", digits);
+}

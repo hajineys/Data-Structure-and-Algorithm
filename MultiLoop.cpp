6b4a528ce3bc2bac2ffbf3fa1e5edd866b8a21c8
hajineys <73228683+hajineys@users.noreply.github.com>
2022-01-10 23:59:36 +0900

    Create MultiLoop.cpp

---
+++ MultiLoop.cpp
@@ -0,0 +1,33 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	int i = 0;
+	int j = 0;
+
+	printf("----------- 곱셈표 ------------\n");
+	printf(" X |");
+	for (int k = 1; k <= 9; k++)
+	{
+		printf("%3d", k);
+	}
+
+	printf("\n");
+	printf("---+---------------------------\n");
+
+	// 행 루프
+	for (i = 1; i <= 9; i++)
+	{
+		printf("%2d |", i);
+
+		// 열 루프
+		for (j = 1; j <= 9; j++)
+		{
+			printf("%3d", i * j);
+		}
+
+		printf("\n");
+	}
+
+	return 0;
+}

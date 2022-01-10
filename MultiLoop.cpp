f2fbba5d1553ed338b20726b4f21d85e5f965861
hajineys <73228683+hajineys@users.noreply.github.com>
2022-01-11 00:09:20 +0900

    다중 루프 사용(곱셈, 덧셈표)
    이중 For문을 활용하여 곱셈과 덧셈표를 만들었다.
    
    이것으로 디버깅을 하며 문자가 하나하나 출력되는걸 보면서 이중 For문의 순서를 파악할 수 있었다.

--- MultiLoop.cpp
+++ MultiLoop.cpp
@@ -5,22 +5,22 @@ int main(void)
 	int i = 0;
 	int j = 0;
 
+#pragma region 곱셈표
 	printf("----------- 곱셈표 ------------\n");
 	printf(" X |");
 	for (int k = 1; k <= 9; k++)
 	{
-		printf("%3d", k);
+		printf("%3d", k);	// 행
 	}
 
-	printf("\n");
-	printf("---+---------------------------\n");
+	printf("\n---+---------------------------\n");
 
-	// 행 루프
+	// 열 루프(세로)
 	for (i = 1; i <= 9; i++)
 	{
 		printf("%2d |", i);
 
-		// 열 루프
+		// 행 루프(가로)
 		for (j = 1; j <= 9; j++)
 		{
 			printf("%3d", i * j);
@@ -28,6 +28,34 @@ int main(void)
 
 		printf("\n");
 	}
+#pragma endregion
+
+	printf("\n");
+
+#pragma region 덧셈표
+	printf("----------- 덧셈표 ------------\n");
+	printf(" + |");
+	for (int k = 1; k <= 9; k++)
+	{
+		printf("%3d", k);	// 행
+	}
+
+	printf("\n---+---------------------------\n");
+
+	// 열 루프(세로) 
+	for (i = 1; i <= 9; i++)
+	{
+		printf("%2d |", i);
+
+		// 행 루프(가로)
+		for (j = 1; j <= 9; j++)
+		{
+			printf("%3d", i + j);
+		}
+
+		printf("\n");
+	}
+#pragma endregion
 
 	return 0;
 }

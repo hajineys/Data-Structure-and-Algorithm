c9901aceb11aee69674c6db62164ce6175d96181
hajineys <73228683+hajineys@users.noreply.github.com>
2022-01-11 22:12:50 +0900

    For 문을 활용한 피라미드 출력

---
+++ Pyramid.cpp
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+
+/// <summary>
+/// for문을 활용하여 피라미드를 출력하세요
+/// </summary>
+int main(void)
+{
+	int height = 5;
+
+	for (int i = 1; i <= height; i++)
+	{
+		// 빈칸 규칙적으로 채워넣기
+		for (int j = 1; j <= height-i; j++)				// j = 1 / 1 <= 5-1 = 4 -> 0000
+		{												// j = 2 / 2 <= 5-2 = 3 -> 000
+			printf(" ");
+		}
+
+		// i의 값이 *를 출력하는 횟수
+		for (int k = 1; k <= (k - 1) * 2 + 1; k++)		// k = 1 / 1 - 1 * 2 + 1 = 1	 *
+		{												// k = 2 / 2 - 1 * 2 + 1 = 3	***
+			printf("*");								// k = 3 / 3 - 1 * 2 + 1 = 5   *****
+		}
+
+		printf("\n");
+	}
+
+	return 0;
+}
